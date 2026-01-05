efe006638cfca40d36bad2d3e6efa7260b521ef4
Jens Langhammer <jens.langhammer@beryju.org>
2026-01-05 15:50:28 +0100

    more

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 source ${GITHUB_ACTION_PATH}/common.sh
 
+temp_dir="${RUNNER_TEMP}/${GITHUB_ACTION}"
+mkdir -p ${temp_dir}
+
+echo "ak_temp_dir=${temp_dir}" >> $GITHUB_ENV
+
+pg_pass=$(openssl rand -base64 32)
+secret_key=$(openssl rand -base64 40)
+
+echo "::add-mask::${pg_pass}"
+echo "::add-mask::${secret_key}"
+
+echo "PG_PASS=${pg_pass}" >> "${ak_temp_dir}/.env"
+echo "AUTHENTIK_SECRET_KEY=${secret_key}" >> "${ak_temp_dir}/.env"
+echo "AUTHENTIK_ERROR_REPORTING__ENABLED=true" >> "${ak_temp_dir}/.env"
+echo "AUTHENTIK_ERROR_REPORTING__ENVIRONMENT=${{ inputs.sentry_env }}" >> "${ak_temp_dir}/.env"
+echo "AUTHENTIK_DISABLE_UPDATE_CHECK=true" >> "${ak_temp_dir}/.env"
+echo "AUTHENTIK_DISABLE_STARTUP_ANALYTICS=true" >> "${ak_temp_dir}/.env"
+echo "CI" >> "${ak_temp_dir}/.env"
+
+
 AUTHENTIK_BOOTSTRAP_TOKEN=$(openssl rand -base64 32)
 echo "::add-mask::${AUTHENTIK_BOOTSTRAP_TOKEN}"
 echo "AUTHENTIK_BOOTSTRAP_TOKEN=${AUTHENTIK_BOOTSTRAP_TOKEN}" >> "${ak_temp_dir}/.env"
--- prepare_temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ${GITHUB_ACTION_PATH}/common.sh
-
-temp_dir="${RUNNER_TEMP}/${GITHUB_ACTION}"
-mkdir -p ${temp_dir}
-
-echo "ak_temp_dir=${temp_dir}" >> $GITHUB_ENV
