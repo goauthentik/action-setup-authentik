eb88b793a4a271abef21b691466ee95ac90f4555
Jens Langhammer <jens.langhammer@beryju.org>
2026-01-05 16:10:34 +0100

    fix wait

--- wait.sh
+++ wait.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 source ${GITHUB_ACTION_PATH}/common.sh
 
+source "${ak_temp_dir}/.env"
+
 while true; do
     echo "Testing authentik with URL ${_AK_WAIT_URL}..."
     status=$(curl \
         -s \
         -o /dev/null \
         -w ''%{http_code}'' \
-        -H "Authorization: Bearer ${_AK_TOKEN}" \
+        -H "Authorization: Bearer ${AUTHENTIK_BOOTSTRAP_TOKEN}" \
         ${_AK_WAIT_URL})
     if [[ $status == "200" ]]; then
         exit 0
